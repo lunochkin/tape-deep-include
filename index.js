7d906082f62afa5573f042b536e97aa1cd62e0b7
Maksim Lunochkin <maxlun86@gmail.com>
2017-02-02 10:30:30 +0300

    tests

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 let test = require('tape');
-let deepEqual = require('deep-equal');
-let defined = require('defined');
 
-
-test.Test.prototype.deepInclude = function (actual, expected, msg, extra) {
-  let isEqual = actual.some(one => {
-    return deepEqual(one, expected, {strict: true});
-  });
-  this._assert(isEqual, {
-    message: defined(msg, 'should include item'),
-    operator: 'deepInclude',
-    actual,
-    expected,
-    extra
-  });
-};
+test.Test.prototype.deepInclude = require('./lib/deepInclude');
